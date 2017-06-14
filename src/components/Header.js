39af1ec59efc58046ab46fa7651307ab3b3b42ce
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2017-06-14 15:48:23 +0700

    done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,21 @@
 import React from 'react'
+import logo from '../assets/lie.png'
+
+const styles = {
+    logo : {
+        marginTop : 30
+    }
+}
 
 class Header extends React.Component {
 
     render() {
         return (
             <div className="card card-inverse card-primary mb-3 text-center">
+                <img className="card-img-top text-center rounded mx-auto d-block" src={logo} alt="logo" height='64' width='64' style={styles.logo}/>
                 <div className="card-block">
-                    <h3 className="card-title">Hoax Fighter</h3>
+                    <h3 className="card-title">LIE</h3>
+                    <h5>Logical Information E-dentification</h5>
                 </div>
             </div>
         )
